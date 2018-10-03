fb4943a8380f9225803c9be97a626ddd9a9c21af
Dell_LG <lzshine@outlook.com>
2018-10-03 22:32:58 +1000

    Finishing the UI coding for the StartPage

---
+++ app/src/main/java/com/example/ddetector/dronedetector/DroneScanningPage.java
@@ -0,0 +1,13 @@
+package com.example.ddetector.dronedetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DroneScanningPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dronescanningpage);
+    }
+}
