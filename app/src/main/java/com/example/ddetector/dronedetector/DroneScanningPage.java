a310d2dbe6ec887931dfa945ff9b3a2fd768e0dc
Dell_LG <lzshine@outlook.com>
2018-10-09 22:27:44 +1100

    Modify the ALL UI. The previous UI is not aligned.

--- app/src/main/java/com/example/ddetector/dronedetector/DroneScanningPage.java
+++ app/src/main/java/com/example/ddetector/dronedetector/DroneScanningPage.java
@@ -1,6 +1,5 @@
 package com.example.ddetector.dronedetector;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,21 +7,13 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
-import android.os.Message;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
@@ -32,116 +23,10 @@ public class DroneScanningPage extends AppCompatActivity {
 
     private WifiManager dronesignalManger;
     private ListView scanninglistView;
-    private Button scanningButton;
-    private int size = 0 ;
     private List<ScanResult> scaningresultsList;
     private ArrayList<String> scanningarrayList =new ArrayList<>();
     private ArrayAdapter scanningarrayAdapter;
-    private Button btnAdd;
     private DatabaseReference ref;
-
-    private Timer wifiscantimer;
-    private TimerTask wifiscantask;
-    private Handler wifiscanhandler;
-
-
-    //DatabaseHelper mDatabaseHelper;
-
-    //add the tag
-//    public static final String TAG = DroneScanningPage.class.getSimpleName();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dronescanningpage);
-//        Intent intent = getIntent();
-//        scanningButton =findViewById(R.id.scanBtn);
-//        scanningButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                scanWifi();
-//
-//            }
- //       });
-
-        //Setup Firebase Database
-        ref = FirebaseDatabase.getInstance().getReference();
-
-        scanninglistView = findViewById(R.id.dronedetectorscanningList);
-        dronesignalManger = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-        if (!dronesignalManger.isWifiEnabled()) {
-            Toast.makeText(this, "Please turn on the WiFi for Drone detection", Toast.LENGTH_LONG).show();
-            dronesignalManger.setWifiEnabled(true);
-        }
-
-        scanningarrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, scanningarrayList);
-        scanninglistView.setAdapter(scanningarrayAdapter);
-
-        wifiscanhandler = new Handler();
-        wifiscantimer = new Timer();
-        wifiscantask = new TimerTask() {
-            @Override
-            public void run() {
-                wifiscanhandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        scanWifi();
-                    }
-                });
-            }
-        };
-        wifiscantimer.schedule(wifiscantask, 15000, 15000);
-
-//        Handler handler = new Handler() {
-//            @Override
-//            public void handleMessage(Message msg) {
-//                super.handleMessage(msg);
-//            }
-//        };
-//
-//        wifiscantask = new TimerTask() {
-//            @Override
-//            public void run () {
-//                Message message = new Message();
-//                message.what = 1;
-//                handler.sendMessage(message);
-//            }
-//        };
-//
-//        timer.schedule(task, 1, 15);
-
-        //Bundle the button with the behavior of creating a databse;
-//        mDatabaseHelper = new DatabaseHelper(this);
-//        btnAdd =(Button) findViewById(R.id.detectorinfo_updating_btn);
-//
-//        btnAdd.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //String newEntry = scanningarrayList.get(1).toString();
-//                String newEntry = "Test";
-//                AddData(newEntry);
-//            }
-//        });
-    }
-
-//    public void AddData (String newEntry) {
-//        boolean insertData =mDatabaseHelper.addData(newEntry);
-//
-//        if(insertData) {
-//            Toast.makeText(this, "Data Sucessful Inserted", Toast.LENGTH_SHORT).show();
-//        } else {
-//            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-
-    private void scanWifi() {
-        scanningarrayList.clear();
-        registerReceiver(dronesignalReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        dronesignalManger.startScan();
-        Toast.makeText(this, "Scaning the drone...", Toast.LENGTH_SHORT).show();
-    }
-
 
     BroadcastReceiver dronesignalReceiver = new BroadcastReceiver() {
         @Override
@@ -159,6 +44,7 @@ public class DroneScanningPage extends AppCompatActivity {
                                     + "Drone Capabilities: " + scanResult.capabilities);
                 scanningarrayAdapter.notifyDataSetChanged();
 
+                //To setup the Firebase Realtime Database Json key and value
                 String systemtime = Long.toString(System.currentTimeMillis());
                 String SSIDdata = scanResult.SSID;
                 String BSSIData = scanResult.BSSID;
@@ -166,19 +52,82 @@ public class DroneScanningPage extends AppCompatActivity {
                 String LevData = Integer.toString(scanResult.level);
                 String CapData = scanResult.capabilities.replaceAll("\\[|\\]", " ");
 
+                //To setup the Json format
+                //We use the "systemtime+BSSID" as the primary key or the first node of the Json
                 ref.child("droneuid_"+systemtime).child(BSSIData)
                         .child("ssid").child(SSIDdata)
                         .child("frequency").child(FreData)
                         .child("level").child(LevData)
-                        .child("Capabilities").child(CapData)
+                        .child("capabilities").child(CapData)
                         .setValue(true);
-
-
-
-                //Print the WIFI info for debugging
-                //Log.v(TAG,"scanningarrayList="+scanningarrayList);
-
             }
         }
     };
+    private Timer wifiscantimer;
+    private TimerTask wifiscantask;
+    private Handler wifiscanhandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dronescanningpage);
+
+        //Currently, We will reserve the Button for setting the Timer value or sending the transactions to the smart contract server in the future.
+//        scanningButton =findViewById(R.id.scanBtn);
+//        scanningButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                scanWifi();
+//
+//            }
+ //       });
+
+        //Setup Firebase Database
+        ref = FirebaseDatabase.getInstance().getReference();
+
+        scanninglistView = findViewById(R.id.dronedetectorscanningList);
+        dronesignalManger = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        //To verify the status of WIFI switch
+        if (!dronesignalManger.isWifiEnabled()) {
+            Toast.makeText(this, "Please turn on the WiFi for Drone detection", Toast.LENGTH_LONG).show();
+            dronesignalManger.setWifiEnabled(true);
+        }
+
+        scanningarrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, scanningarrayList);
+        scanninglistView.setAdapter(scanningarrayAdapter);
+
+        //To define the WIFI scan Timer, We choose 15 seconds now
+        //IF there is a requirement of setting the Timer value on the UI in future
+        //We can add a TextView in the "activity_dronescanningpage.xml"and bundle its input Timer value.
+        wifiscanhandler = new Handler();
+        wifiscantimer = new Timer();
+        wifiscantask = new TimerTask() {
+            @Override
+            public void run() {
+                wifiscanhandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scanWifi();
+                    }
+                });
+            }
+        };
+        //15000(15 SECONDS) = 15 *1000 milliseconds
+        //DELAY:fixed-delay execution(repeated)
+        //PERIOD:beginning after the specified delay(repeated)
+        //Note that,If you set the schedule(...,delay 1000, period 15000).
+        //It follows this excution pattern, Open App......1s......15s......1s......15s......1s......15s......
+        //Why? Due to the "DELAY" aims at "fixed-delay execution" in each loop, not only the first loop
+        //So when you run the wifi scanning, you need to wait the "xx" delay value, then you can see the WIFI results for the first time.
+        // It is not a Bug.
+        wifiscantimer.schedule(wifiscantask, 15000, 15000);
+    }
+
+    private void scanWifi() {
+        scanningarrayList.clear();
+        registerReceiver(dronesignalReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        dronesignalManger.startScan();
+        Toast.makeText(this, "Scaning the drone...", Toast.LENGTH_SHORT).show();
+    }
 }
