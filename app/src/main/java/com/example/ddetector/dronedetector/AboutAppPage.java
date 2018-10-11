4c049a9bce180ce63ad1323161c6047b5caaddb4
Dell_LG <lzshine@outlook.com>
2018-10-11 14:32:56 +1100

    Adding the Android device location(GPS provider) features

--- app/src/main/java/com/example/ddetector/dronedetector/AboutAppPage.java
+++ app/src/main/java/com/example/ddetector/dronedetector/AboutAppPage.java
@@ -1,11 +1,8 @@
 package com.example.ddetector.dronedetector;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
 
 public class AboutAppPage extends AppCompatActivity {
 
@@ -14,6 +11,13 @@ public class AboutAppPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutapp_page);
 
+        TextView aboutaootextView = (TextView) findViewById(R.id.ID_AboutAppPage_TXT_Introduction);
+        aboutaootextView.setText("Note that:"+"\n"
+                                  +"-To support the WIFI scan feature, you should make sure you have grant the Android LOCATIONS permission for this APP;"+"\n"
+                                  +"-The WIFI scan interval Timer is 15 seconds and written in the code, it maybe support to set the Timer vaule on the APP in future version."
+                                  +"\n\n\n"
+                                  +"Version 0.1"+"\n"
+                                  +"Copyright@2018 Deakin University"+"\n"
+                                  +"All rights reserved.");
     }
-
 }
--- app/src/main/java/com/example/ddetector/dronedetector/DetectorInfoPage.java
+++ app/src/main/java/com/example/ddetector/dronedetector/DetectorInfoPage.java
@@ -1,63 +1,95 @@
 package com.example.ddetector.dronedetector;
 
 import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
+
+import java.util.Date;
 
 
 public class DetectorInfoPage extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detectorinfo_page);
 
-        //Get the Android MAC address as the Detector UID
+        //
+        TextView detectorprovidertxtView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorProvider);
+        TextView detectornametxtView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorName);
+        TextView detectorHealthyInfoView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorHealthy);
+        final TextView detectorLocationInfoView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorLocations);
+        detectorLocationInfoView.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        //Get the Android device MAC address as the Detector UID
         WifiManager wifimanager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = wifimanager.getConnectionInfo();
         String macAddress = info.getMacAddress();
 
-        //
+        //Get the Android device location Info
+        LocationManager locationmanager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationlistener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Date devicetime = new Date(System.currentTimeMillis());
+                detectorLocationInfoView.append("\n"+
+                                                "Device Time: " + devicetime +"\n"+
+                                                "Provider: " + location.getProvider() + "\n" +
+                                                "Longitude: " + location.getLongitude() + "\n" +
+                                                "Latitude: " + location.getLatitude() + "\n"  +
+                                                "Altitude: " + location.getAltitude() + "\n" +
+                                                "Accuracy: " + location.getAccuracy() + "\n" +
+                                                "Speed: " + location.getSpeed() + "\n" );
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) { }
+            @Override
+            public void onProviderEnabled(String provider) { }
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        try {
+            locationmanager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, locationlistener);
+        } catch (SecurityException e) {
+            detectorLocationInfoView.setText(e.getMessage());
+        }
 
-//        BatteryManager batterymanager = (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
-//
-//        int level = batterymanager.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-//        int scale = batterymanager.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-//
-//        float batteryPct = level / (float)scale;
-
-
-        //
-//        HardwarePropertiesManager hardwaremanager = (HardwarePropertiesManager) getSystemService(Context.HARDWARE_PROPERTIES_SERVICE);
-//
-//        float[] temperatures = hardwaremanager.getDeviceTemperatures( HardwarePropertiesManager.DEVICE_TEMPERATURE_CPU, HardwarePropertiesManager.TEMPERATURE_CURRENT);
-
-
-
-
-        TextView detectorprovidertxtView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorProvider);
-        detectorprovidertxtView.setText("DETECTOR PROVIDER" + "\n" + "Deakin Univeristy Burwood Campus"); //set text for text view
-
-        TextView detectornametxtView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorName);
-        detectornametxtView.setText("DETECTOR UID" + "\n" + "Detector BSSID is " + macAddress);
-
-        TextView detectorHealthyInfoView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorHealthy);
-        detectorHealthyInfoView.setText("DETECTOR HEALTHY INFO" + "\n"
-                                        + "CPU: " + "\n"
-                                        + "MEMORY: " +  "\n"
-                                        + "BATTERY: " ); //set text for text view
-
-        TextView detectorLocationInfoView = (TextView) findViewById(R.id.ID_DetectorInfoPage_TXT_DetectorLocations);
-        detectorLocationInfoView.setText("DETECTOR LOCATION INFO" + "\n"
-                                         + "Latitude: " + "\n"
-                                         + "Longitude: " ); //set text for text view
+        detectorprovidertxtView.setText("DETECTOR PROVIDER"+"\n"+"Deakin Univeristy Burwood Campus"); //set text for text view
+        detectornametxtView.setText("DETECTOR UID"+"\n"+"Detector BSSID is "+macAddress);
+        detectorHealthyInfoView.setText("D CHARGING STATION INFO"+"\n"
+                +"D Charging Station Balance: "+"\n"
+                +"D Charging Station Status: ");
     }
 
-//    @Override
+    //
+    //        BatteryManager batterymanager = (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
+    //
+    //        int level = batterymanager.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+    //        int scale = batterymanager.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    //
+    //        float batteryPct = level / (float)scale;
+
+    //
+    //        HardwarePropertiesManager hardwaremanager = (HardwarePropertiesManager) getSystemService(Context.HARDWARE_PROPERTIES_SERVICE);
+    //
+    //        float[] temperatures = hardwaremanager.getDeviceTemperatures( HardwarePropertiesManager.DEVICE_TEMPERATURE_CPU, HardwarePropertiesManager.TEMPERATURE_CURRENT);
+
+
+    //    @Override
     ////    public void onCreate() {
     ////        BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
     ////            int scale = -1;
